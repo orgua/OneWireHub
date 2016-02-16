021e86755fa84b7acdd78999c87a3822f994e08e
orgua <orgua@users.noreply.github.com>
2016-02-16 09:03:52 +0100

    reduce scope of sensor-vars

--- DS2890.h
+++ DS2890.h
@@ -3,11 +3,11 @@
 class DS2890 : public OneWireItem
 {
 private:
+    uint8_t regs;
+    uint8_t potion;
+
     bool duty(OneWireHub *hub);
 
 public:
     DS2890(uint8_t ID1, uint8_t ID2, uint8_t ID3, uint8_t ID4, uint8_t ID5, uint8_t ID6, uint8_t ID7);
-
-    uint8_t regs;
-    uint8_t potion;
 };
