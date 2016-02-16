6b3f4dd4d05c41be155e2b2d3887462b9347f86f
orgua <splingmael@gmail.com>
2016-02-16 09:07:57 +0100

    revert reduce var-scope for ds2413

--- DS2413.h
+++ DS2413.h
@@ -3,14 +3,14 @@
 class DS2413 : public OneWireItem
 {
 private:
+    bool duty(OneWireHub *hub);
+
+public:
     bool AState;  // sensed.A
     bool ALatch;  // PIO.A
     bool BState;  // sensed.B
     bool BLatch;  // PIO.B
 
-    bool duty(OneWireHub *hub);
-
-public:
     DS2413(uint8_t ID1, uint8_t ID2, uint8_t ID3, uint8_t ID4, uint8_t ID5, uint8_t ID6, uint8_t ID7);
 
     virtual void ReadState();
