4dafa7080e09c03fc28dc9440bca36bced33b2e8
orgua <splingmael@gmail.com>
2016-02-18 09:01:21 +0100

    improve examples with documentation

--- examples/OneWireHubTest/OneWireHubTest.ino
+++ examples/OneWireHubTest/OneWireHubTest.ino
@@ -24,7 +24,8 @@ const uint8_t OneWire_PIN   = 8;
 OneWireHub  hub = OneWireHub(OneWire_PIN);
 auto ds18B20a = DS18B20(0x28, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);      // Work - Digital Thermometer
 auto ds18B20b = DS18B20(0x28, 0x0D, 0x01, 0x08, 0x0B, 0x02, 0x00);
-auto ds2401   = DS2401( 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 );    // Work - Serial Number
+auto ds2401a  = DS2401( 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0A );    // Work - Serial Number
+auto ds2401b  = DS2401( 0x01, 0x0D, 0x02, 0x04, 0x00, 0x01, 0x0B );    // Work - Serial Number
 // auto ds2405   = DS2405( 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 );    //      - Single adress switch
 // auto ds2408   = DS2408( 0x29, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 );    //      - 8-Channel Addressable Switch
 auto ds2413   = DS2413( 0x3A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 );    // Work - Dual channel addressable switch
@@ -36,15 +37,13 @@ auto ds2890   = DS2890( 0x2C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 );    // Work -
 
 bool blinking()
 {
-    const uint32_t  interval    = 500;          // interval at which to blink (milliseconds)
+    const  uint32_t interval    = 500;          // interval at which to blink (milliseconds)
     static uint32_t nextMillis  = millis();     // will store next time LED will updated
 
-    uint32_t currentMillis = millis();
-
-    if (currentMillis > nextMillis)
+    if (millis() > nextMillis)
     {
-        static uint8_t ledState = LOW;          // ledState used to set the LED
-        nextMillis = currentMillis + interval;  // save the last time you blinked the LED
+        nextMillis += interval;             // save the next time you blinked the LED
+        static uint8_t ledState = LOW;      // ledState used to set the LED
         if (ledState == LOW)    ledState = HIGH;
         else                    ledState = LOW;
         digitalWrite(led_PIN, ledState);
@@ -55,34 +54,47 @@ bool blinking()
 
 void setup()
 {
-    // Debug
     Serial.begin(115200);
     Serial.println("OneWire-Hub Test with various Sensors");
 
-    // put your setup code here, to run once:
-    hub.attach(ds18B20a); // TODO: can there only be 4 sensors at once?
+    if (0)
+    {
+        // include a simple test-case
+        hub.waitForRequest(false); // old code had infinite loop here
+        Serial.print("Attach DS2401: ");
+        Serial.println(hub.attach(ds2401a));
+        hub.waitForRequest(false); // old code had infinite loop here
+        Serial.print("Detach DS2401: ");
+        Serial.println(hub.detach(ds2401a));
+        hub.waitForRequest(false);
+        Serial.println("Testcase done");
+    }
+
+    // Setup OneWire
+    ds18B20a.setTemp(10);
+    hub.attach(ds18B20a); // TODO: BUG - can there only be 4 sensors at once?
     hub.attach(ds18B20b);
-    hub.attach(ds2401);
+    hub.attach(ds2401a);
+    //hub.attach(ds2401b);
     hub.attach(ds2413);
     //hub.attach(ds2450); // TODO: still breaks the communication
     //hub.attach(ds2890);
-    Serial.println(hub.calck_mask());
+
     Serial.println("config done");
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
+    // following function must be called periodically
     hub.waitForRequest(false);
 
-    // Blink
+    // Blink triggers the state-change
     if (blinking())
     {
         // DS18B20
         static float temperature = 20.0;
         temperature += 0.1;
         if (temperature > 40.0) temperature = 10.0;
-        //ds18B20a.setTemp(21);
         ds18B20b.setTemp(temperature);
         Serial.println(temperature);
 
