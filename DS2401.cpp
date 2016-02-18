a047cbf3c5458e12bc84a6d7e8de39bee5bf9b53
orgua <orgua@users.noreply.github.com>
2016-02-18 19:40:54 +0100

    experiment with pre-defined familycode, so the ID1 at init is ignored

--- DS2401.cpp
+++ DS2401.cpp
@@ -3,10 +3,6 @@
 
 const bool dbg_DS2401 = 0; // give debug messages for this sensor
 
-DS2401::DS2401(uint8_t ID1, uint8_t ID2, uint8_t ID3, uint8_t ID4, uint8_t ID5, uint8_t ID6, uint8_t ID7) : OneWireItem(ID1, ID2, ID3, ID4, ID5, ID6, ID7)
-{
-}
-
 bool DS2401::duty(OneWireHub *hub)
 {
     const uint8_t done = hub->recv();
