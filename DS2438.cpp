e18c1fd4691f9421a6c5ae63d96226b4161b0593
orgua <splingmael@gmail.com>
2016-02-20 19:05:00 +0100

    implement and test ds2438

--- DS2438.cpp
+++ DS2438.cpp
@@ -4,49 +4,32 @@
 DS2438::DS2438(uint8_t ID1, uint8_t ID2, uint8_t ID3, uint8_t ID4, uint8_t ID5, uint8_t ID6, uint8_t ID7) : OneWireItem(ID1, ID2, ID3, ID4, ID5, ID6, ID7)
 {
 
-    for (int i = 0; i < sizeof(memory); ++i)
+    for (uint8_t i = 0; i < (PAGE_EMU_COUNT*8); ++i)
         memory[i] = MemDS2438[i]; // 0x00;
 
     setTemp(static_cast<uint8_t>(80));
-/*
-  // Flags
-  memory[0] = DS2438_IAD | DS2438_CA | DS2438_EE | DS2438_AD;
-
-  // Temp
-  memory[1] = 0x10;
-  memory[2] = 0x19;
-
-  // Volt
-  memory[3] = 0xF4;
-  memory[4] = 0x01;
-
-  // Cur
-  memory[5] = 0x40;
-  memory[6] = 0x00;
-*/
 }
 
 bool DS2438::duty(OneWireHub *hub)
 {
     uint8_t done = hub->recv();
     uint8_t page;
-    uint8_t b; // TODO: unused
     uint8_t crc;
+    uint8_t garbage[8];
 
     switch (done)
     {
-        // Convert T
-        case 0x44:
-            //hub->sendBit(1);
+        case 0x44: // Convert T
+            hub->sendBit(1);
             if (dbg_sensor) Serial.println("DS2438 : Convert T");
             break;
 
-            // Write Scratchpad
-        case 0x4E:
-            // page
+        case 0x4E: // Write Scratchpad
             page = hub->recv();
-            hub->recvData(&memory[page * 8], 8);
-
+            if (page >= PAGE_EMU_COUNT)
+                hub->recvData(garbage, 8);
+            else
+                hub->recvData(&memory[page * 8], 8);
             if (dbg_sensor)
             {
                 Serial.print("DS2438 : Write Scratchpad - Page:");
@@ -54,17 +37,13 @@ bool DS2438::duty(OneWireHub *hub)
             }
             break;
 
-            // Convert V
-        case 0xB4:
-            //hub->sendBit(1);
+        case 0xB4: // Convert V
+            hub->sendBit(1);
             if (dbg_sensor) Serial.println("DS2438 : Convert V");
             break;
 
-            // Recall Memory
-        case 0xB8:
-            // page
+        case 0xB8: // Recall Memory
             page = hub->recv();
-
             if (dbg_sensor)
             {
                 Serial.print("DS2438 : Recall Memory - Page:");
@@ -72,25 +51,30 @@ bool DS2438::duty(OneWireHub *hub)
             }
             break;
 
-            // Read Scratchpad
-        case 0xBE:
-            // page
+        case 0xBE: // Read Scratchpad
             page = hub->recv();
-
-            //offset = page*8;
-
-            // crc
-            crc = crc8(&memory[page * 8], 8);
-
-            hub->sendData(&memory[page * 8], 8);
+            if (page >= PAGE_EMU_COUNT)
+            {
+                crc = crc8(garbage, 8);
+                hub->sendData(garbage, 8);
+            }
+            else
+            {
+                crc = crc8(&memory[page * 8], 8);
+                hub->sendData(&memory[page * 8], 8);
+            }
             hub->send(crc);
-
             if (dbg_sensor)
             {
                 Serial.print("DS2438 : Read Scratchpad - Page:");
                 Serial.println(page, HEX);
             }
             break;
+
+        case 0x48: // copy scratchpad
+            page = hub->recv() & 0x07;
+            hub->sendBit(1);
+            if (dbg_sensor) Serial.println("DS2438 : Copy Scratchpad");
 
         default:
             if (dbg_HINT)
@@ -110,7 +94,7 @@ void DS2438::setTemp(const float temp_degC)
     if (temp_degC < 0)
         value = -value;
 
-    memory[1] = uint8_t(value);
+    memory[1] = static_cast<uint8_t>(value&0xF8);
     memory[2] = uint8_t(value >> 8);
 
     if (temp_degC < 0)
@@ -127,14 +111,15 @@ void DS2438::setTemp(const uint8_t temp_degC)
 }
 
 
-void DS2438::setVolt(const uint16_t voltage_10mV) // TODO: is this more than 10 bit? maybe use a mask
+void DS2438::setVolt(const uint16_t voltage_10mV)
 {
     memory[3] = uint8_t(voltage_10mV);
     memory[4] = uint8_t((voltage_10mV >> 8) & static_cast<uint8_t>(0x03));
 }
 
-void DS2438::setCurr(const uint16_t value) // TODO: signed 9 bit
+void DS2438::setCurr(const int16_t value) // signed 11 bit
 {
     memory[5] = uint8_t(value);
     memory[6] = uint8_t((value >> 8) & static_cast<uint8_t>(0x03));
+    if (value<0) memory[6] |= 0xFC; // all upper bits (7:2) are the sign
 }
