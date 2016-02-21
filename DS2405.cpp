9212b9df36f86751b8ebe9c4b894af7d2f11ead3
orgua <orgua@users.noreply.github.com>
2016-02-21 13:39:04 +0100

    implement ds2405

--- DS2405.cpp
+++ DS2405.cpp
@@ -3,24 +3,22 @@
 
 DS2405::DS2405(uint8_t ID1, uint8_t ID2, uint8_t ID3, uint8_t ID4, uint8_t ID5, uint8_t ID6, uint8_t ID7) : OneWireItem(ID1, ID2, ID3, ID4, ID5, ID6, ID7)
 {
+    pin_state = 0;
 }
 
 bool DS2405::duty(OneWireHub *hub)
 {
-    uint8_t done = hub->recv();
+    // IC uses weird bus-features to operate.
 
-    switch (done)
+    pin_state = !pin_state;
+    hub->sendBit(pin_state);
+
+    if (dbg_HINT)
     {
-        // Active-Only Search ROM
-        // EC - @@@
-        default:
-            if (dbg_HINT)
-            {
-                Serial.print("DS2405=");
-                Serial.println(done, HEX); // 00 - Set ON
-            }
-            break;
+        Serial.print("DS2405=");
+        Serial.println(pin_state, HEX);
     }
+
     return true;
 }
 
---
+++ examples/Sensor_DS2405/Sensor_DS2405.ino
@@ -0,0 +1,55 @@
+/*
+ *    Example-Code that emulates a DS2413 Dual channel addressable switch
+ *    Tested with https://github.com/PaulStoffregen/OneWire --> still untested
+ */
+
+#include "OneWireHub.h"
+#include "DS2405.h"  // Dual channel addressable switch
+
+const uint8_t led_PIN       = 13;         // the number of the LED pin
+const uint8_t OneWire_PIN   = 8;
+
+auto hub    = OneWireHub(OneWire_PIN);
+auto ds2405 = DS2405( DS2405::family_code, 0x00, 0x0D, 0x02, 0x04, 0x00, 0x05 );    // Work - Dual channel addressable switch
+
+bool blinking()
+{
+    const  uint32_t interval    = 1000;          // interval at which to blink (milliseconds)
+    static uint32_t nextMillis  = millis();     // will store next time LED will updated
+
+    if (millis() > nextMillis)
+    {
+        nextMillis += interval;             // save the next time you blinked the LED
+        static uint8_t ledState = LOW;      // ledState used to set the LED
+        if (ledState == LOW)    ledState = HIGH;
+        else                    ledState = LOW;
+        digitalWrite(led_PIN, ledState);
+        return 1;
+    }
+    return 0;
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("OneWire-Hub DS2413 Dual channel addressable switch");
+
+    // Setup OneWire
+    hub.attach(ds2405);
+    ds2405.setState(1);
+
+    Serial.println("config done");
+}
+
+void loop()
+{
+    // following function must be called periodically
+    hub.waitForRequest(false);
+
+    // Blink triggers the state-change
+    if (blinking())
+    {
+        Serial.print(" PinState: ");
+        Serial.println(ds2405.readState());
+    }
+}
