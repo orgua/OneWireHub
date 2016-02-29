382438449d2583dd86002083d651ec72b7d571cf
orgua <splingmael@gmail.com>
2016-02-29 10:03:00 +0100

    update examples

--- examples/debug-benchmark-timings/debug-benchmark-timings.ino
+++ examples/debug-benchmark-timings/debug-benchmark-timings.ino
@@ -3,9 +3,14 @@
  *
  *    - Var1: use micros()
  *    - Var2: count ticks
+ *
+ *    Results (Atmega328@16MHz)
+ *    - 16bit-ticks-loop takes 11 ticks to repeat, 32bit takes 14 ticks
+ *    - results on pin are the same, early leaving the loop takes 690 ns
+ *    -
+ *
  */
 
-#define TIMESLOT_WAIT_RETRY_COUNT       (microsecondsToClockCycles(1000))
 #define DIRECT_READ(base, mask)         (((*(base)) & (mask)) ? 1 : 0)
 #define DIRECT_MODE_INPUT(base, mask)   ((*((base)+1)) &= ~(mask))
 #define DIRECT_MODE_OUTPUT(base, mask)  ((*((base)+1)) |= (mask))
@@ -17,9 +22,18 @@ void setup()
     Serial.begin(115200);
     Serial.println("Test-Code for a suitable timing function");
 
+    // setup timers
     uint32_t time_start, time_stop;
 
-    /// timer test //////////////////////////////////////////////////
+    // setup direct pin-access
+    uint8_t pin_bitMask = digitalPinToBitMask(8);
+    volatile uint8_t *baseReg;
+    baseReg = portInputRegister(digitalPinToPort(8));
+    volatile uint8_t *reg asm("r30") = baseReg;
+    uint8_t value = 0;
+
+    /// Benchmark for a timer test //////////////////////////////////////////////////
+
     uint16_t counter = 0;
     time_stop = micros() + 1000;
     while (micros() < time_stop)
@@ -29,18 +43,14 @@ void setup()
     Serial.print("Benchmark: 1ms got us ");
     Serial.print(counter);
     Serial.println(" * (micros(), 32bit check, 32bit increment)");
+    Serial.flush();
 
     /// Benchmark for waitTimeSlot-Code //////////////////////////////
 
-    uint8_t pin_bitMask = digitalPinToBitMask(8);
-    volatile uint8_t *baseReg;
-    baseReg = portInputRegister(digitalPinToPort(8));
-    volatile uint8_t *reg asm("r30") = baseReg;
     DIRECT_WRITE_LOW(reg, pin_bitMask);
     DIRECT_MODE_OUTPUT(reg, pin_bitMask); // put it low, so it always reads zero
-    const uint8_t value = 0;
 
-    uint16_t retries = TIMESLOT_WAIT_RETRY_COUNT;
+    uint16_t retries = microsecondsToClockCycles(1000);
     Serial.print("1000ms translate to ");
     Serial.print(retries);
     Serial.println(" ticks.");
@@ -48,12 +58,14 @@ void setup()
 
     //////////// 16bit /////////////////////////////////////
 
+    retries = microsecondsToClockCycles(1000);
     time_start = micros();
     while (DIRECT_READ(reg, pin_bitMask) == value)
     {
             if (--retries == 0) break;
     }
     time_stop = micros();
+
     Serial.print("16bit Loop took ");
     Serial.print(time_stop - time_start);
     Serial.println(" us. ");
@@ -61,16 +73,76 @@ void setup()
 
     //////////// 32bit /////////////////////////////////////
 
-    uint32_t retries32 = TIMESLOT_WAIT_RETRY_COUNT;
+    uint32_t retries32 = microsecondsToClockCycles(1000);
     time_start = micros();
     while (DIRECT_READ(reg, pin_bitMask) == value)
     {
         if (--retries32 == 0) break;
     }
     time_stop = micros();
+
     Serial.print("32bit Loop took ");
     Serial.print(time_stop - time_start);
     Serial.println(" us. ");
+    Serial.flush();
+
+    //////////// test timings on the Pin /////////////////////////////////////
+    // use logic probe or osci to confirm programming
+    // 1.0ms: High, then low
+    // 0.1ms: High, then Low
+
+    DIRECT_WRITE_HIGH(reg, pin_bitMask);
+    value   = 1;
+    retries = microsecondsToClockCycles(1000);
+    while (DIRECT_READ(reg, pin_bitMask) == value)
+    {
+        if (--retries == 0) break;
+    }
+
+    DIRECT_WRITE_LOW(reg, pin_bitMask);
+    value   = 0;
+    retries = microsecondsToClockCycles(1000);
+    while (DIRECT_READ(reg, pin_bitMask) == value)
+    {
+        if (--retries == 0) break;
+    }
+
+    DIRECT_WRITE_HIGH(reg, pin_bitMask);
+    value   = 1;
+    retries = microsecondsToClockCycles(100);
+    while (DIRECT_READ(reg, pin_bitMask) == value)
+    {
+        if (--retries == 0) break;
+    }
+
+    DIRECT_WRITE_LOW(reg, pin_bitMask);
+    value   = 0;
+    retries = microsecondsToClockCycles(100);
+    while (DIRECT_READ(reg, pin_bitMask) == value)
+    {
+        if (--retries == 0) break;
+    }
+
+    //////////// try to exit the loop early on success //////////////////////////////
+
+    DIRECT_WRITE_HIGH(reg, pin_bitMask);
+    value   = 0;
+    retries = microsecondsToClockCycles(10);
+    while (DIRECT_READ(reg, pin_bitMask) == value)
+    {
+        if (--retries == 0) break;
+    }
+
+    DIRECT_WRITE_LOW(reg, pin_bitMask);
+    value   = 1;
+    retries = microsecondsToClockCycles(10);
+    while (DIRECT_READ(reg, pin_bitMask) == value)
+    {
+        if (--retries == 0) break;
+    }
+
+    // toggle pin one last time to time the last slot
+    DIRECT_WRITE_HIGH(reg, pin_bitMask);
 
 }
 
