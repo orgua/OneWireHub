2b8b8d4b68e6e81e84425165998922c81e483c8a
orgua <splingmael@gmail.com>
2016-09-18 01:09:46 +0200

    update example

--- examples/DS2433_EEPROM/DS2433_EEPROM.ino
+++ examples/DS2433_EEPROM/DS2433_EEPROM.ino
@@ -12,6 +12,7 @@ const uint8_t led_PIN = 13;         // the number of the LED pin
 const uint8_t OneWire_PIN = 8;
 
 auto hub = OneWireHub(OneWire_PIN);
+auto ds2433 = DS2433(DS2433::family_code, 0x01, 0x01, 0x33, 0x24, 0xD0, 0x00);
 
 void setup()
 {
