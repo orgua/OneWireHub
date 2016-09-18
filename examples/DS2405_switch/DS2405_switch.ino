81642850214bb5d1b6f3ba746c0d0dd408dc1a39
orgua <orgua@users.noreply.github.com>
2016-09-18 12:31:40 +0200

    update expample ds2405

--- examples/DS2405_switch/DS2405_switch.ino
+++ examples/DS2405_switch/DS2405_switch.ino
@@ -3,6 +3,7 @@
  *
  *    Tested with
  *    - DS9490R-Master, atmega328@16MHz as Slave
+ *    - Note: feedback to master is unclear, ds9490 does not read after matchRom
  */
 
 #include "OneWireHub.h"
@@ -14,23 +15,6 @@ const uint8_t OneWire_PIN   = 8;
 auto hub    = OneWireHub(OneWire_PIN);
 auto ds2405 = DS2405( DS2405::family_code, 0x00, 0x0D, 0x02, 0x04, 0x00, 0x05 );    // Work - Dual channel addressable switch
 
-bool blinking()
-{
-    const  uint32_t interval    = 1000;          // interval at which to blink (milliseconds)
-    static uint32_t nextMillis  = millis();     // will store next time LED will updated
-
-    if (millis() > nextMillis)
-    {
-        nextMillis += interval;             // save the next time you blinked the LED
-        static uint8_t ledState = LOW;      // ledState used to set the LED
-        if (ledState == LOW)    ledState = HIGH;
-        else                    ledState = LOW;
-        digitalWrite(led_PIN, ledState);
-        return 1;
-    }
-    return 0;
-}
-
 void setup()
 {
     Serial.begin(115200);
@@ -40,20 +24,29 @@ void setup()
 
     // Setup OneWire
     hub.attach(ds2405);
-    ds2405.setState(1);
+    ds2405.setState(0);
 
     Serial.println("config done");
 }
 
 void loop()
 {
+    static bool switch_state = 0;
+    
     // following function must be called periodically
     hub.poll();
 
-    // Blink triggers the state-change
-    if (blinking())
+    if (switch_state != ds2405.readState())
     {
+        switch_state = ds2405.readState();
+
+        // visual feedback and printF
         Serial.print(" PinState: ");
-        Serial.println(ds2405.readState());
+        Serial.println(switch_state);
+
+        if (switch_state)       digitalWrite(led_PIN, HIGH);
+        else                    digitalWrite(led_PIN, LOW);
     }
+
+
 }
