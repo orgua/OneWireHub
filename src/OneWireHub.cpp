0804d274de90ec8741cf73f0c48a925bd6a0b52f
orgua <orgua@users.noreply.github.com>
2016-10-16 21:09:37 +0200

    fix 1ms high-state during first attachment

--- src/OneWireHub.cpp
+++ src/OneWireHub.cpp
@@ -748,7 +748,7 @@ timeOW_t OneWireHub::waitLoopsWhilePinIs(volatile timeOW_t retries, const bool p
 void OneWireHub::waitLoops1ms(void)
 {
 #if USE_GPIO_DEBUG
-    constexpr timeOW_t loops_1ms = timeUsToLoops(uint16_t(VALUE1k * VALUE1k));
+    constexpr timeOW_t loops_1ms = timeUsToLoops(uint16_t(VALUE1k));
     timeOW_t loops_left = 1;
     while (loops_left)
     {
