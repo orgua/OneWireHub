1cac1af2dd52badb8cbd5d7dc97c1eadec535109
orgua <splingmael@gmail.com>
2016-10-27 09:43:54 +0200

    tuning for the timings

--- src/OneWireHub_config.h
+++ src/OneWireHub_config.h
@@ -7,10 +7,10 @@
 
 // INFO: had to go with a define because some compilers use constexpr as simple const --> massive problems
 #define HUB_SLAVE_LIMIT     8 // set the limit of the hub HERE, max is 32 devices
-#define OVERDRIVE_ENABLE    0 // support overdrive for the slaves
+#define OVERDRIVE_ENABLE    1 // support overdrive for the slaves
 
 #define USE_SERIAL_DEBUG    0 // give debug messages when printError() is called (be aware! it may produce heisenbugs, timing is critical)
-#define USE_GPIO_DEBUG      0 // is a better alternative to serial debug (see readme.md for info)
+#define USE_GPIO_DEBUG      1 // is a better alternative to serial debug (see readme.md for info)
 
 constexpr uint8_t  GPIO_DEBUG_PIN   { 7 }; // digital pin
 constexpr uint32_t REPETITIONS      { 5000 }; // for measuring the loop-delay --> 10000L takes ~110ms on atmega328p@16Mhz
@@ -44,16 +44,16 @@ constexpr uint16_t OVERDRIVE_TIME_PRESENCE_MIN       =    8;
 constexpr uint16_t OVERDRIVE_TIME_PRESENCE_MAX       =   32;
 //
 
-constexpr uint16_t OVERDRIVE_TIME_SLOT_MAX           =   20; //
+constexpr uint16_t OVERDRIVE_TIME_SLOT_MAX           =   30; //
 
-constexpr uint16_t OVERDRIVE_TIME_READ_MIN           =   10; // was 30
-constexpr uint16_t OVERDRIVE_TIME_READ_MAX           =   16; //
-constexpr uint16_t OVERDRIVE_TIME_WRITE_ZERO         =   13; //
+constexpr uint16_t OVERDRIVE_TIME_READ_MIN           =    4; //
+constexpr uint16_t OVERDRIVE_TIME_READ_MAX           =   10; //
+constexpr uint16_t OVERDRIVE_TIME_WRITE_ZERO         =    8; //
 
 // VALUES FOR STATIC ASSERTS
 constexpr uint16_t ONEWIRE_TIME_VALUE_MAX            = ONEWIRE_TIME_MSG_HIGH_TIMEOUT;
 #if OVERDRIVE_ENABLE
-constexpr uint16_t ONEWIRE_TIME_VALUE_MIN            = OVERDRIVE_TIME_PRESENCE_MIN;
+constexpr uint16_t ONEWIRE_TIME_VALUE_MIN            = OVERDRIVE_TIME_READ_MIN;
 #else
 constexpr uint16_t ONEWIRE_TIME_VALUE_MIN            = ONEWIRE_TIME_PRESENCE_TIMEOUT;
 #endif
