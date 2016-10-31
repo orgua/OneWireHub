b5821429e3f33d83ecfefad04536bf596398a792
orgua <orgua@users.noreply.github.com>
2016-11-01 00:38:21 +0100

    clean up ds2502

--- src/DS2502.cpp
+++ src/DS2502.cpp
@@ -2,11 +2,23 @@
 
 DS2502::DS2502(uint8_t ID1, uint8_t ID2, uint8_t ID3, uint8_t ID4, uint8_t ID5, uint8_t ID6, uint8_t ID7) : OneWireItem(ID1, ID2, ID3, ID4, ID5, ID6, ID7)
 {
-    static_assert(sizeof(memory) < 256,  "Implementation does not cover the whole address-space");
+    static_assert(sizeof(memory) < 256, "Implementation does not cover the whole address-space");
 
-    if      (ID1 == 0x11)   sizeof_memory = (64 < sizeof(memory)) ? 64 : sizeof(memory); // autorecognize the ds2501 with smaller mem-size
-    else if (ID1 == 0x91)   sizeof_memory = (64 < sizeof(memory)) ? 64 : sizeof(memory); // autorecognize the ds2501 with smaller men-size
-    else                    sizeof_memory = sizeof(memory);
+    switch (ID1)
+    {
+        case 0x11:  // DS2501
+        case 0x91:
+            sizeof_memory = 64;
+            break;
+
+        case 0x09:  // DS2502
+        case 0x89:
+            sizeof_memory = 128;
+            break;
+
+        default:
+            sizeof_memory = 64;
+    };
 
     clearMemory();
     clearStatus();
@@ -14,8 +26,8 @@ DS2502::DS2502(uint8_t ID1, uint8_t ID2, uint8_t ID3, uint8_t ID4, uint8_t ID5, 
 
 void DS2502::duty(OneWireHub *hub)
 {
-    uint16_t reg_TA; // Target address
-    uint8_t  reg_RA = 0, cmd, data, crc = 0; // redirected address, command, data, crc
+    uint16_t reg_TA, reg_RA = 0; // Target address
+    uint8_t  cmd, data, crc = 0; // redirected address, command, data, crc
 
     if (hub->recv(&cmd))  return;
     crc = crc8(&cmd,1,crc);
@@ -44,7 +56,7 @@ void DS2502::duty(OneWireHub *hub)
             while (reg_RA < sizeof_memory)
             {
                 crc = 0; // reInit CRC and send data
-                reg_RA = uint8_t((reg_TA & ~page_mask) + (1 << 5));
+                reg_RA = uint16_t((reg_TA & ~PAGE_MASK) + (1 << 5));
                 for (uint16_t i = reg_TA; i < reg_RA; ++i)
                 {
                     uint8_t j = translateRedirection(i);
@@ -139,7 +151,7 @@ void DS2502::duty(OneWireHub *hub)
 
 void DS2502::clearMemory(void)
 {
-    memset(&memory[0], static_cast<uint8_t>(0xFF), sizeof_memory);
+    memset(&memory[0], static_cast<uint8_t>(0xFF), SIZE_MEM);
 };
 
 void DS2502::clearStatus(void)
@@ -150,7 +162,7 @@ void DS2502::clearStatus(void)
 
 bool DS2502::writeMemory(const uint8_t* source, const uint8_t length, const uint8_t position)
 {
-    const uint16_t _length = (position + length >= sizeof_memory) ? (sizeof_memory - position) : length;
+    const uint16_t _length = (position + length >= SIZE_MEM) ? (SIZE_MEM - position) : length; // TODO: dirty hack, just changed sizeofmem to SIZE_MEM
     memcpy(&memory[position],source,_length);
     return (_length==length);
 };
@@ -163,9 +175,10 @@ bool DS2502::checkProtection(const uint16_t reg_address)
 
 uint8_t DS2502::translateRedirection(const uint16_t reg_address)
 {
+    if (reg_address >= SIZE_MEM) return (SIZE_MEM-1); // out of bound is translated to last byte
     uint8_t reg_index = uint8_t(1) + uint8_t(reg_address >> 5);
     uint8_t reg_offset = (status[reg_index] == 0xFF) ? uint8_t(reg_address) : ((~status[reg_index])<<5);
-    return ((reg_offset & ~page_mask) | uint8_t(reg_address & page_mask));
+    return ((reg_offset & ~PAGE_MASK) | uint8_t(reg_address & PAGE_MASK));
 };
 
 bool DS2502::redirectPage(const uint8_t page_source, const uint8_t page_dest)
