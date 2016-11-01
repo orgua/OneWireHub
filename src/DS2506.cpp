65648ba01115c8bd9cdd0f63fc0a45860a5856e3
orgua <splingmael@gmail.com>
2016-11-01 22:46:43 +0100

    add and extend ds2506

--- src/DS2506.cpp
+++ src/DS2506.cpp
@@ -2,7 +2,7 @@
 
 DS2506::DS2506(uint8_t ID1, uint8_t ID2, uint8_t ID3, uint8_t ID4, uint8_t ID5, uint8_t ID6, uint8_t ID7) : OneWireItem(ID1, ID2, ID3, ID4, ID5, ID6, ID7)
 {
-    static_assert(sizeof(memory) < 256, "Implementation does not cover the whole address-space");
+    static_assert(sizeof(memory) <= 0xFFFF, "Implementation does not cover the whole address-space");
 
     switch (ID1)
     {
@@ -28,32 +28,46 @@ DS2506::DS2506(uint8_t ID1, uint8_t ID2, uint8_t ID3, uint8_t ID4, uint8_t ID5, 
 
 void DS2506::duty(OneWireHub *hub)
 {
-    uint16_t reg_TA, reg_RA = 0; // Target address
-    uint8_t  cmd, data, crc = 0; // redirected address, command, data, crc
+    uint16_t reg_TA, reg_RA = 0, crc = 0; // Target address
+    uint8_t  cmd, data; // redirected address, command, data, crc
 
-    if (hub->recv(&cmd))  return;
-    crc = crc8(&cmd,1,crc);
-
-    if (hub->recv(reinterpret_cast<uint8_t *>(&reg_TA),2))  return;
-    crc = crc8(reinterpret_cast<uint8_t *>(&reg_TA),2,crc);
+    if (hub->recv(&cmd,1,crc))  return;
+    if (hub->recv(reinterpret_cast<uint8_t *>(&reg_TA),2,crc))  return;
 
     switch (cmd)
     {
         case 0xF0:      // READ MEMORY
-            if (hub->send(&crc))        break;
-
-            crc = 0; // reInit CRC and send data
-            for (uint16_t i = reg_TA; i < sizeof_memory; ++i)
+            while (reg_TA <= sizeof_memory)
             {
-                uint8_t j = translateRedirection(i);
-                if (hub->send(&memory[j])) return;
-                crc = crc8(&memory[j],1,crc);
+                const uint16_t real_TA = translateRedirection(reg_TA); // TODO: seems to be without redirection? check
+                const uint8_t  length  = PAGE_SIZE - uint8_t(reg_TA & PAGE_MASK);
+                if (hub->send(&memory[real_TA],length,crc)) return;
+                reg_TA += length;
             };
-            hub->send(&crc);
-            break; // datasheed says we should return all 1s, send(255), till reset, nothing to do here, 1s are passive
+            crc = ~crc; // normally crc16 is sent ~inverted
+            hub->send(reinterpret_cast<uint8_t *>(&crc),2);
+            break; // datasheed says we should return 1s, till reset, nothing to do here
 
-        case 0xC3:      // READ DATA (like 0xF0, but repeatedly till the end of page with following CRC)
-            if (hub->send(&crc)) break;
+        case 0xA5:      // EXTENDED READ MEMORY (with redirection)
+            while (reg_TA <= sizeof_memory)
+            {
+                const uint8_t  redir   = getRedirection(reg_TA);
+                if (hub->send(&redir,1,crc))                        return;
+                crc = ~crc; // normally crc16 is sent ~inverted
+                hub->send(reinterpret_cast<uint8_t *>(&crc),2); // send crc of (cmd,TA,redir) at first, then only crc of (redir)
+                crc=0;
+                const uint16_t real_TA = translateRedirection(reg_TA);
+                const uint8_t  length  = PAGE_SIZE - uint8_t(reg_TA & PAGE_MASK);
+                if (hub->send(&memory[real_TA],length,crc))         break;
+                crc = ~crc; // normally crc16 is sent ~inverted
+                if (hub->send(reinterpret_cast<uint8_t *>(&crc),2)) break;
+                reg_TA += length;
+                crc=0;
+            };
+            break; // datasheed says we should return 1s, till reset, nothing to do here
+
+        case 0xC3:      // READ DATA (like 0xF0, but repeatedly till the end of page with following CRC) // TODO
+            if (hub->send(reinterpret_cast<uint8_t *>(&crc),2)) break;
 
             while (reg_RA < sizeof_memory)
             {
@@ -61,38 +75,33 @@ void DS2506::duty(OneWireHub *hub)
                 reg_RA = uint16_t((reg_TA & ~PAGE_MASK) + (1 << 5));
                 for (uint16_t i = reg_TA; i < reg_RA; ++i)
                 {
-                    uint8_t j = translateRedirection(i);
+                    uint16_t j = translateRedirection(i);
                     if (hub->send(&memory[j])) return;
-                    crc = crc8(&memory[j], 1, crc);
+                    //crc = crc8(&memory[j], 1, crc);
                 };
-
-                if (hub->send(&crc)) break;
+                crc = ~crc; // normally crc16 is sent ~inverted
+                if (hub->send(reinterpret_cast<uint8_t *>(&crc),2)) break;
                 reg_TA = reg_RA;
             };
-            break; // datasheed says we should return all 1s, send(255), till reset, nothing to do here, 1s are passive
+            break; // datasheed says we should return 1s, till reset, nothing to do here
 
-        case 0xAA:      // READ STATUS // TODO: nearly same code as 0xF0, but with status[] instead of memory[]
-            if (hub->send(&crc)) break;
-
-            crc = 0; // reInit CRC and send data
-            for (uint16_t i = reg_TA; i < sizeof(status); ++i)
+        case 0xAA:      // READ STATUS // TODO
             {
-                // TODO: redirection
-                if (hub->send(&status[i])) return;
-                crc = crc8(&status[i],1,crc);
+                const uint16_t real_TA = reg_TA & uint16_t(STATUS_SIZE-1);
+                const uint8_t  length  = uint8_t(8 - (reg_TA & 7));
+                if (hub->send(&status[real_TA],length,crc)) return;
             };
+            crc = ~crc; // normally crc16 is sent ~inverted
+            hub->send(reinterpret_cast<uint8_t *>(&crc),2);
+            break; // datasheed says we should return 1s, till reset, nothing to do here
 
-            hub->send(&crc);
-            // datasheed says we should return all 1s, send(255), till reset, nothing to do here, 1s are passive
-            break;
-
-        case 0x0F:      // WRITE MEMORY
+        case 0x0F:      // WRITE MEMORY // TODO
             if (reg_TA > sizeof_memory)     break; // check for valid address
 
             if (hub->recv(&data))           break;
-            crc = crc8(&data,1,crc);
-
-            if (hub->send(&crc))            break;
+            //crc = crc8(&data,1,crc);
+            crc = ~crc; // normally crc16 is sent ~inverted
+            if (hub->send(reinterpret_cast<uint8_t *>(&crc),2))            break;
 
             if (checkProtection(reg_TA))    break;
 
@@ -106,8 +115,8 @@ void DS2506::duty(OneWireHub *hub)
             {
                 if (hub->recv(&data))       break;
                 crc = crc8(&data,1,reinterpret_cast<uint8_t *>(&reg_TA)[0]);
-
-                if (hub->send(&crc))        break;
+                crc = ~crc; // normally crc16 is sent ~inverted
+                if (hub->send(reinterpret_cast<uint8_t *>(&crc),2))        break;
 
                 reg_RA = translateRedirection(reg_TA);
                 if (!checkProtection(reg_RA))
@@ -119,13 +128,13 @@ void DS2506::duty(OneWireHub *hub)
             };
             break;
 
-        case 0x55:      // WRITE STATUS
+        case 0x55:      // WRITE STATUS // TODO
             if (reg_TA > sizeof(status))    break; // check for valid address
 
             if (hub->recv(&data))           break;
-            crc = crc8(&data,1,crc);
-
-            if (hub->send(&crc))            break;
+            //crc = crc8(&data,1,crc);
+            crc = ~crc; // normally crc16 is sent ~inverted
+            if (hub->send(reinterpret_cast<uint8_t *>(&crc),2))            break;
 
             status[reg_TA] &= data; // like EPROM-Mode
 
@@ -135,9 +144,9 @@ void DS2506::duty(OneWireHub *hub)
             while (reg_TA < sizeof(status))
             {
                 if (hub->recv(&data))       break;
-                crc = crc8(&data,1,reinterpret_cast<uint8_t *>(&reg_TA)[0]);
-
-                if (hub->send(&crc))        break;
+                //crc = crc8(&data,1,reinterpret_cast<uint8_t *>(&reg_TA)[0]);
+                crc = ~crc; // normally crc16 is sent ~inverted
+                if (hub->send(reinterpret_cast<uint8_t *>(&crc),2))        break;
 
                 status[reg_TA] &= data; // like EPROM-Mode
                 if (hub->send(&status[reg_TA])) break;
@@ -153,7 +162,7 @@ void DS2506::duty(OneWireHub *hub)
 
 void DS2506::clearMemory(void)
 {
-    memset(&memory[0], static_cast<uint8_t>(0xFF), SIZE_MEM);
+    memset(&memory[0], static_cast<uint8_t>(0xFF), MEM_SIZE);
 };
 
 void DS2506::clearStatus(void)
@@ -162,10 +171,17 @@ void DS2506::clearStatus(void)
     status[sizeof(status)-1] = 0x00; // last byte should be always zero
 };
 
-bool DS2506::writeMemory(const uint8_t* source, const uint8_t length, const uint8_t position)
+bool DS2506::writeMemory(const uint8_t* source, const uint16_t length, const uint16_t position)
 {
-    const uint16_t _length = (position + length >= SIZE_MEM) ? (SIZE_MEM - position) : length; // TODO: dirty hack, just changed sizeofmem to SIZE_MEM
+    const uint16_t _length = (position + length >= MEM_SIZE) ? (MEM_SIZE - position) : length;
     memcpy(&memory[position],source,_length);
+    return (_length==length);
+};
+
+bool DS2506::readMemory(const uint8_t* destination, const uint16_t length, const uint16_t position)
+{
+    const uint16_t _length = (position + length >= MEM_SIZE) ? (MEM_SIZE - position) : length;
+    memcpy(destination,&memory[position],_length);
     return (_length==length);
 };
 
@@ -175,18 +191,25 @@ bool DS2506::checkProtection(const uint16_t reg_address)
     return ((status[0] & (1<<reg_index)) == 0);
 };
 
-uint8_t DS2506::translateRedirection(const uint16_t reg_address)
+uint8_t DS2506::getRedirection(const uint16_t reg_address)
 {
-    if (reg_address >= SIZE_MEM) return (SIZE_MEM-1); // out of bound is translated to last byte
-    uint8_t reg_index = uint8_t(1) + uint8_t(reg_address >> 5);
-    uint8_t reg_offset = (status[reg_index] == 0xFF) ? uint8_t(reg_address) : ((~status[reg_index])<<5);
+    if (reg_address >= MEM_SIZE) return uint8_t(~(reg_address & MEM_MASK)>>5); // out of bound is translated to last byte
+    const uint16_t reg_index  = uint8_t(1) + (reg_address >> 5);
+    return (status[reg_index]);
+};
+
+uint16_t DS2506::translateRedirection(const uint16_t reg_address) // TODO: extended read mem implies that redirection is recursive
+{
+    if (reg_address >= MEM_SIZE) return (reg_address & MEM_MASK); // out of bound is translated to last byte
+    const uint16_t reg_index  = uint8_t(1) + (reg_address >> 5);
+    const uint16_t reg_offset = (status[reg_index] == 0xFF) ? (reg_address) : ((~status[reg_index])<<5);
     return ((reg_offset & ~PAGE_MASK) | uint8_t(reg_address & PAGE_MASK));
 };
 
 bool DS2506::redirectPage(const uint8_t page_source, const uint8_t page_dest)
 {
-    if (page_source > 3) return false;
-    if (page_dest > 3) return false;
+    if (page_source >= PAGE_SIZE) return false;
+    if (page_dest >= PAGE_SIZE) return false;
 
     status[page_source + 1] = (page_dest == page_source) ? uint8_t(0xFF) : ~page_dest; // datasheet dictates this, so no page can be redirected to page 0
     return true;
@@ -194,7 +217,7 @@ bool DS2506::redirectPage(const uint8_t page_source, const uint8_t page_dest)
 
 bool DS2506::protectPage(const uint8_t page, const bool status_protected)
 {
-    if (page > 3) return false;
+    if (page > PAGE_SIZE) return false;
 
     status[0] &= ~(1<<page);
     status[0] |= ((!status_protected)&0x01)<<page;
--- src/DS2506.h
+++ src/DS2506.h
@@ -9,30 +9,43 @@ class DS2506 : public OneWireItem
 {
 private:
 
-    static constexpr uint8_t PAGE_COUNT  = 4;
-    static constexpr uint8_t PAGE_SIZE   = 32;
-    static constexpr uint8_t PAGE_MASK   = 0b00011111;
-    static constexpr uint8_t SIZE_MEM    = PAGE_COUNT * PAGE_SIZE;
+    static constexpr uint16_t MEM_SIZE_PROPOSE  = 256;
 
-    uint8_t     memory[SIZE_MEM]; // 4 pages of 32 bytes
+    static constexpr uint8_t  PAGE_SIZE         = 32;
+    static constexpr uint16_t PAGE_COUNT        = MEM_SIZE_PROPOSE / PAGE_SIZE; // TODO: CLEAN UP,
+    static constexpr uint8_t  PAGE_MASK         = 0b00011111;
+
+    static constexpr uint16_t MEM_SIZE          = PAGE_COUNT * PAGE_SIZE;
+    static constexpr uint16_t MEM_MASK          = MEM_SIZE - 1;
+
+    static constexpr uint16_t STATUS_SEGMENT    = (PAGE_COUNT/8);
+    static constexpr uint16_t STATUS_SIZE       = PAGE_COUNT + (3*STATUS_SEGMENT);
+
+    uint8_t     memory[MEM_SIZE]; // 4 pages of 32 bytes
     uint16_t    sizeof_memory;
-    uint8_t     status[8]; // eprom status bytes
+    uint8_t     status[STATUS_SIZE]; // eprom status bytes
+    // PAGE_COUNT/8 bytes -> Page write protection
+    // PAGE_COUNT/8 bytes -> Redirection write protection
+    // PAGE_COUNT/8 bytes -> Page used status (written one)
+    // PAGE_COUNT bytes   -> Redirection to page, 0xFF if valid, ones complement (xFD is page 2)
 
-    void    clearStatus(void);
-    bool    checkProtection(const uint16_t reg_address = 0);
-    uint8_t translateRedirection(const uint16_t reg_address = 0);
+    void     clearStatus(void);
+    bool     checkProtection(const uint16_t reg_address = 0);
+
+    uint8_t  getRedirection(const uint16_t reg_address);
+    uint16_t translateRedirection(const uint16_t reg_address = 0);
 
 public:
-    static constexpr uint8_t family_code = 0x13;
+    static constexpr uint8_t family_code = 0x0F;
 
-    // Initializer will get stuck in an infinite loop when you choose a sensor
     DS2506(uint8_t ID1, uint8_t ID2, uint8_t ID3, uint8_t ID4, uint8_t ID5, uint8_t ID6, uint8_t ID7);
 
     void duty(OneWireHub *hub);
 
     void clearMemory(void);
 
-    bool writeMemory(const uint8_t* source, const uint8_t length, const uint8_t position = 0);
+    bool writeMemory(const uint8_t* source, const uint16_t length, const uint16_t position = 0);
+    bool readMemory(const uint8_t* destination, const uint16_t length, const uint16_t position);
 
     bool redirectPage(const uint8_t page_source, const uint8_t page_dest);
     bool protectPage(const uint8_t page, const bool status_protected);
