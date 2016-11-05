981a6e5e4756672e98e4d54d538b86704b5bb78f
orgua <orgua@users.noreply.github.com>
2016-11-05 17:06:47 +0100

    fix and improve ds2502 and ds2506

--- examples/DS2506_EEPROM/DS2506_EEPROM.ino
+++ examples/DS2506_EEPROM/DS2506_EEPROM.ino
@@ -29,6 +29,7 @@ void setup()
     hub.attach(ds2505);
     hub.attach(ds2506);
 
+    // the following code is just to show basic functions, can be removed any time
     Serial.println("Test Redirection Protection");
     Serial.println(ds2506.getRedirectionProtection(2)); // Zero
     ds2506.setRedirectionProtection(2);
@@ -47,8 +48,18 @@ void setup()
     Serial.println(ds2506.getPageRedirection(2)); // should show the same as before --> is protected
 
     Serial.println("Test Write Data to page 4");
+    Serial.println(ds2506.getPageUsed(4));
     const uint8_t mem_dummy[] = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
     ds2506.writeMemory(mem_dummy, sizeof(mem_dummy), 4*32);
+    Serial.println(ds2506.getPageUsed(4));
+
+    Serial.println("Test Write Data to protected page 0 -> is possible, only affects master");
+    Serial.println(ds2506.getPageUsed(0));
+    Serial.println(ds2506.getPageProtection(0));
+    ds2506.setPageProtection(0);
+    Serial.println(ds2506.getPageProtection(0));
+    ds2506.writeMemory(mem_dummy, sizeof(mem_dummy), 16); // write in second half of page
+    Serial.println(ds2506.getPageUsed(0));
 
     Serial.println("config done");
 }
--- src/DS2502.cpp
+++ src/DS2502.cpp
@@ -2,38 +2,38 @@
 
 DS2502::DS2502(uint8_t ID1, uint8_t ID2, uint8_t ID3, uint8_t ID4, uint8_t ID5, uint8_t ID6, uint8_t ID7) : OneWireItem(ID1, ID2, ID3, ID4, ID5, ID6, ID7)
 {
-    static_assert(sizeof(memory) < 256, "Implementation does not cover the whole address-space");
-
-    switch (ID1)
-    {
-        case 0x11:  // DS2501
-        case 0x91:
-            sizeof_memory = 64;
-            break;
-
-        case 0x09:  // DS2502
-        case 0x89:
-            sizeof_memory = 128;
-            break;
-
-        default:
-            sizeof_memory = 64;
-    };
+    static_assert(MEM_SIZE < 256, "Implementation does not cover the whole address-space");
 
     clearMemory();
     clearStatus();
+
+    if ((ID1 == 0x11) || (ID1 == 0x91))
+    {
+        // when set to DS2501, the upper two memory pages are not accessible, always read 0xFF
+        for (uint8_t page = 2; page < PAGE_COUNT; ++page)
+        {
+            setPageUsed(page);
+            setPageProtection(page);
+            sizeof_memory = 64;
+        };
+    }
+    else
+    {
+        sizeof_memory = 128;    // must be DS2502 then
+    };
 };
 
 void DS2502::duty(OneWireHub *hub)
 {
-    uint16_t reg_TA, reg_RA = 0; // Target address
-    uint8_t  cmd, data, crc = 0; // redirected address, command, data, crc
+    uint8_t  reg_TA[2], cmd, data, crc = 0; // Target address, redirected address, command, data, crc
 
     if (hub->recv(&cmd))  return;
     crc = crc8(&cmd,1,crc);
 
-    if (hub->recv(reinterpret_cast<uint8_t *>(&reg_TA),2))  return;
-    crc = crc8(reinterpret_cast<uint8_t *>(&reg_TA),2,crc);
+    if (hub->recv(reg_TA,2))  return;
+    crc = crc8(reg_TA,2,crc);
+
+    if (reg_TA[1]) return; // upper byte of target adress should not contain any data
 
     switch (cmd)
     {
@@ -41,106 +41,84 @@ void DS2502::duty(OneWireHub *hub)
             if (hub->send(&crc))        break;
 
             crc = 0; // reInit CRC and send data
-            for (uint16_t i = reg_TA; i < sizeof_memory; ++i)
+            for (uint8_t i = reg_TA[0]; i < sizeof_memory; ++i)
             {
-                uint8_t j = translateRedirection(i);
-                if (hub->send(&memory[j])) return;
-                crc = crc8(&memory[j],1,crc);
+                const uint8_t reg_RA = translateRedirection(i);
+                if (hub->send(&memory[reg_RA])) return;
+                crc = crc8(&memory[reg_RA],1,crc);
             };
             hub->send(&crc);
-            break; // datasheed says we should return all 1s, send(255), till reset, nothing to do here, 1s are passive
+            break; // datasheet says we should return all 1s, send(255), till reset, nothing to do here, 1s are passive
 
         case 0xC3:      // READ DATA (like 0xF0, but repeatedly till the end of page with following CRC)
             if (hub->send(&crc)) break;
 
-            while (reg_RA < sizeof_memory)
+            while (reg_TA[0] < sizeof_memory)
             {
                 crc = 0; // reInit CRC and send data
-                reg_RA = uint16_t((reg_TA & ~PAGE_MASK) + (1 << 5));
-                for (uint16_t i = reg_TA; i < reg_RA; ++i)
+                const uint8_t reg_EA = (reg_TA[0] & ~PAGE_MASK) + PAGE_SIZE; // End Address
+                for (uint8_t i = reg_TA[0]; i < reg_EA; ++i)
                 {
-                    uint8_t j = translateRedirection(i);
-                    if (hub->send(&memory[j])) return;
-                    crc = crc8(&memory[j], 1, crc);
+                    const uint8_t reg_RA = translateRedirection(i);
+                    if (hub->send(&memory[reg_RA])) return;
+                    crc = crc8(&memory[reg_RA], 1, crc);
                 };
 
                 if (hub->send(&crc)) break;
-                reg_TA = reg_RA;
+                reg_TA[0] = reg_EA;
             };
-            break; // datasheed says we should return all 1s, send(255), till reset, nothing to do here, 1s are passive
+            break; // datasheet says we should return all 1s, send(255), till reset, nothing to do here, 1s are passive
 
         case 0xAA:      // READ STATUS // TODO: nearly same code as 0xF0, but with status[] instead of memory[]
             if (hub->send(&crc)) break;
 
             crc = 0; // reInit CRC and send data
-            for (uint16_t i = reg_TA; i < sizeof(status); ++i)
+            for (uint8_t i = reg_TA[0]; i < STATUS_SIZE; ++i)
             {
-                // TODO: redirection
                 if (hub->send(&status[i])) return;
                 crc = crc8(&status[i],1,crc);
             };
-
             hub->send(&crc);
-            // datasheed says we should return all 1s, send(255), till reset, nothing to do here, 1s are passive
-            break;
+            break; // datasheet says we should return all 1s, send(255), till reset, nothing to do here, 1s are passive
 
         case 0x0F:      // WRITE MEMORY
-            if (reg_TA > sizeof_memory)     break; // check for valid address
-
-            if (hub->recv(&data))           break;
-            crc = crc8(&data,1,crc);
-
-            if (hub->send(&crc))            break;
-
-            if (checkProtection(reg_TA))    break;
-
-            reg_RA = translateRedirection(reg_TA);
-            memory[reg_RA] &= data; // like EPROM-Mode
-
-            if (hub->send(&memory[reg_RA])) break;
-
-            reg_TA++;
-            while (reg_TA < sizeof_memory)
+            while (reg_TA[0] < sizeof_memory)
             {
                 if (hub->recv(&data))       break;
-                crc = crc8(&data,1,reinterpret_cast<uint8_t *>(&reg_TA)[0]);
+                crc = crc8(&data,1,crc);
 
                 if (hub->send(&crc))        break;
 
-                reg_RA = translateRedirection(reg_TA);
-                if (!checkProtection(reg_RA))
+                const uint8_t reg_RA = translateRedirection(reg_TA[0]);
+
+                if (getPageProtection(reg_TA[0]))
                 {
-                    memory[reg_RA] &= data; // like EPROM-Mode
+                    const uint8_t mem_zero = 0x00; // send dummy data
+                    if (hub->send(&mem_zero)) break;
+                }
+                else
+                {
+                    memory[reg_RA] &= data; // EPROM-Mode
+                    setPageUsed(reg_RA);
                     if (hub->send(&memory[reg_RA])) break;
                 };
-                reg_TA++;
+                crc = ++reg_TA[0];
             };
             break;
 
         case 0x55:      // WRITE STATUS
-            if (reg_TA > sizeof(status))    break; // check for valid address
-
-            if (hub->recv(&data))           break;
-            crc = crc8(&data,1,crc);
-
-            if (hub->send(&crc))            break;
-
-            status[reg_TA] &= data; // like EPROM-Mode
-
-            if (hub->send(&status[reg_TA])) break;
-
-            reg_TA++;
-            while (reg_TA < sizeof(status))
+            while (reg_TA[0] < STATUS_SIZE)
             {
                 if (hub->recv(&data))       break;
-                crc = crc8(&data,1,reinterpret_cast<uint8_t *>(&reg_TA)[0]);
+                crc = crc8(&data,1,crc);
 
                 if (hub->send(&crc))        break;
 
-                status[reg_TA] &= data; // like EPROM-Mode
-                if (hub->send(&status[reg_TA])) break;
+                data = writeStatus(reg_TA[0], data);
 
-                reg_TA++;
+                if (hub->send(&data)) break;
+
+                crc = ++reg_TA[0];
             };
             break;
 
@@ -149,53 +127,94 @@ void DS2502::duty(OneWireHub *hub)
     };
 };
 
+uint8_t DS2502::translateRedirection(const uint8_t source_address)
+{
+    const uint8_t  source_page    = static_cast<uint8_t >(source_address >> 5);
+    const uint8_t  destin_page    = getPageRedirection(source_page);
+    if (destin_page == 0x00)        return source_address;
+    const uint8_t destin_address  = (source_address & PAGE_MASK) | (destin_page << 5);
+    return destin_address;
+};
+
 void DS2502::clearMemory(void)
 {
-    memset(&memory[0], static_cast<uint8_t>(0xFF), SIZE_MEM);
+    memset(memory, static_cast<uint8_t>(0xFF), MEM_SIZE);
 };
 
 void DS2502::clearStatus(void)
 {
-    memset(&status[0], static_cast<uint8_t>(0xFF), SIZE_STATUS);
-    status[SIZE_STATUS-1] = 0x00; // last byte should be always zero
+    memset(status, static_cast<uint8_t>(0xFF), STATUS_SIZE);
+    status[STATUS_FACTORYP] = 0x00; // last byte should be always zero
 };
 
 bool DS2502::writeMemory(const uint8_t* source, const uint8_t length, const uint8_t position)
 {
-    const uint16_t _length = (position + length >= SIZE_MEM) ? (SIZE_MEM - position) : length; // TODO: dirty hack, just changed sizeofmem to MEM_SIZE
+    if (position >= MEM_SIZE) return false;
+    const uint16_t _length = (position + length >= MEM_SIZE) ? (MEM_SIZE - position) : length;
     memcpy(&memory[position],source,_length);
+
+    const uint8_t page_start = static_cast<uint8_t>(position >> 5);
+    const uint8_t page_stop  = static_cast<uint8_t>((position + _length) >> 5);
+    for (uint8_t page = page_start; page <= page_stop; page++) setPageUsed(page);
+
     return (_length==length);
 };
 
-bool DS2502::checkProtection(const uint16_t reg_address)
+bool DS2502::readMemory(uint8_t* const destination, const uint8_t length, const uint8_t position)
 {
-    uint8_t reg_index = uint8_t(reg_address >> 5);
-    return ((status[0] & (1<<reg_index)) == 0);
+    if (position >= MEM_SIZE) return 0;
+    const uint16_t _length = (position + length >= MEM_SIZE) ? (MEM_SIZE - position) : length;
+    memcpy(destination,&memory[position],_length);
+    return (_length==length);
 };
 
-uint8_t DS2502::translateRedirection(const uint16_t reg_address)
+
+uint8_t DS2502::readStatus(const uint8_t address)
 {
-    if (reg_address >= SIZE_MEM) return (SIZE_MEM-1); // out of bound is translated to last byte
-    uint8_t reg_index = uint8_t(1) + uint8_t(reg_address >> 5);
-    uint8_t reg_offset = (status[reg_index] == 0xFF) ? uint8_t(reg_address) : ((~status[reg_index])<<5);
-    return ((reg_offset & ~PAGE_MASK) | uint8_t(reg_address & PAGE_MASK));
+    if (address >= STATUS_SIZE)     return 0xFF;
+    return status[address];
 };
 
-bool DS2502::redirectPage(const uint8_t page_source, const uint8_t page_dest)
+uint8_t DS2502::writeStatus(const uint8_t address, const uint8_t value)
 {
-    if (page_source > 3) return false;
-    if (page_dest > 3) return false;
+    if (address < STATUS_UNDEF_B1)  status[address] &= value; // writing is allowed only here
+    return status[address];
+};
 
-    status[page_source + 1] = (page_dest == page_source) ? uint8_t(0xFF) : ~page_dest; // datasheet dictates this, so no page can be redirected to page 0
+void DS2502::setPageProtection(const uint8_t page)
+{
+    if (page < PAGE_COUNT)          status[STATUS_WP_PAGES] &= ~(uint8_t(1<<page));
+};
+
+bool DS2502::getPageProtection(const uint8_t page)
+{
+    if (page >= PAGE_COUNT) return true;
+    return !(status[STATUS_WP_PAGES] & uint8_t(1<<page));
+};
+
+void DS2502::setPageUsed(const uint8_t page)
+{
+    if (page < PAGE_COUNT)  status[STATUS_WP_PAGES] &= ~(uint8_t(1<<(page+4)));
+};
+
+bool DS2502::getPageUsed(const uint8_t page)
+{
+    if (page >= PAGE_COUNT) return true;
+    return !(status[STATUS_WP_PAGES] & uint8_t(1<<(page+4)));
+};
+
+
+bool DS2502::setPageRedirection(const uint8_t page_source, const uint8_t page_destin)
+{
+    if (page_source >= PAGE_COUNT)  return false; // really available
+    if (page_destin >= PAGE_COUNT)  return false; // virtual mem of the device
+
+    status[page_source + STATUS_PG_REDIR] = (page_destin == page_source) ? uint8_t(0xFF) : ~page_destin; // datasheet dictates this, so no page can be redirected to page 0
     return true;
 };
 
-bool DS2502::protectPage(const uint8_t page, const bool status_protected)
+uint8_t DS2502::getPageRedirection(const uint8_t page)
 {
-    if (page > 3) return false;
-
-    status[0] &= ~(1<<page);
-    status[0] |= ((!status_protected)&0x01)<<page;
-
-    return true;
+    if (page >= PAGE_COUNT) return 0x00;
+    return ~(status[page + STATUS_PG_REDIR]); // TODO: maybe invert this in ReadStatus and safe some Operations? Redirection is critical and often done
 };
