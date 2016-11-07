2890f9a8398624e63b75965a994b9f48d652f9b2
orgua <orgua@users.noreply.github.com>
2016-11-08 00:54:57 +0100

    add documentation to timing value

--- src/OneWireHub_config.h
+++ src/OneWireHub_config.h
@@ -18,30 +18,30 @@ constexpr uint32_t REPETITIONS      { 5000 }; // for measuring the loop-delay --
 //  arrays contain the normal timing value and the overdrive-value, the literal "_us" converts the value right away to a usable unit
 //  should be --> datasheet
 //  was       --> shagrat-legacy
+
+// Reset: every low-state of the master between MIN & MAX microseconds will be recognized as a Reset
 constexpr timeOW_t ONEWIRE_TIME_RESET_TIMEOUT        =  5000_us;          // for not hanging to long in reset-detection, lower value is better for more responsive applications, but can miss resets
 constexpr timeOW_t ONEWIRE_TIME_RESET_MIN[2]         = { 430_us, 48_us }; // should be 480
 constexpr timeOW_t ONEWIRE_TIME_RESET_MAX[2]         = { 960_us, 80_us }; // from ds2413
 
-constexpr timeOW_t ONEWIRE_TIME_PRESENCE_TIMEOUT     =    20_us;          // probe measures 25us
+// Presence: slave waits TIMEOUT and emits a low state after the reset with ~MIN length, if the bus stays low after that and exceeds MAX the hub will issue an error
+constexpr timeOW_t ONEWIRE_TIME_PRESENCE_TIMEOUT     =    20_us;          // probe measures 25us, duration of high state between reset and presence
 constexpr timeOW_t ONEWIRE_TIME_PRESENCE_MIN[2]      = { 160_us,  8_us }; // was 125
 constexpr timeOW_t ONEWIRE_TIME_PRESENCE_MAX[2]      = { 480_us, 32_us }; // should be 280, was 480
 
+
 constexpr timeOW_t ONEWIRE_TIME_MSG_HIGH_TIMEOUT     = 15000_us;          // there can be these inactive / high timeperiods after reset / presence, this value defines the timeout for these
-constexpr timeOW_t ONEWIRE_TIME_SLOT_MAX[2]          = { 135_us, 30_us }; // should be 120
+constexpr timeOW_t ONEWIRE_TIME_SLOT_MAX[2]          = { 135_us, 30_us }; // should be 120, measured from falling edge to next falling edge
 
 // read and write from the viewpoint of the slave!!!!
-constexpr timeOW_t ONEWIRE_TIME_READ_MIN[2]          = {  20_us,  4_us }; // should be 15, was 30
-constexpr timeOW_t ONEWIRE_TIME_READ_MAX[2]          = {  60_us, 10_us }; //
-constexpr timeOW_t ONEWIRE_TIME_WRITE_ZERO[2]        = {  30_us,  8_us }; //
-
+constexpr timeOW_t ONEWIRE_TIME_READ_MIN[2]          = {  20_us,  4_us }; // should be 15, was 30, says when it is safe to read a valid bit
+constexpr timeOW_t ONEWIRE_TIME_READ_MAX[2]          = {  60_us, 10_us }; // low states (zeros) of a master should not exceed this time in a slot
+constexpr timeOW_t ONEWIRE_TIME_WRITE_ZERO[2]        = {  30_us,  8_us }; // the hub holds a zero for this long
 
 // VALUES FOR STATIC ASSERTS
 constexpr timeOW_t ONEWIRE_TIME_VALUE_MAX            = ONEWIRE_TIME_MSG_HIGH_TIMEOUT;
-#if OVERDRIVE_ENABLE
-constexpr timeOW_t ONEWIRE_TIME_VALUE_MIN            = ONEWIRE_TIME_READ_MIN[2];
-#else
-constexpr timeOW_t ONEWIRE_TIME_VALUE_MIN            = ONEWIRE_TIME_READ_MIN[1];
-#endif
+constexpr timeOW_t ONEWIRE_TIME_VALUE_MIN            = ONEWIRE_TIME_READ_MIN[OVERDRIVE_ENABLE];
+
 
 /////////////////////////////////////////////////////
 // END OF CONFIG ////////////////////////////////////
